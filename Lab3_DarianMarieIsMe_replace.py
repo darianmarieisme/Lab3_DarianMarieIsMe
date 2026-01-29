7263cf4667524d4090d5aefae614c82c4b4ee40d
Darian Bruce <darianmariebruce@gmail.com>
2026-01-29 18:46:05 -0500

    started part 3 wrote header and imported list

---
+++ Lab3_DarianMarieIsMe_replace.py
@@ -0,0 +1,10 @@
+'''Lab 3- Programming Assignment - Part 3
+Darian Marie Bruce
+The purpose of this program is to import the list from part 2 and replace
+one of the string items from this list with binoculars and use slice notation
+01/29/2026'''
+
+# Importing the list
+
+from Lab3_DarianMarieIsMe_add import items
+
