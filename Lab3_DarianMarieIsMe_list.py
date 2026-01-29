939602f00b1567a23bab2cb117972b13e848b082
Darian Bruce <darianmariebruce@gmail.com>
2026-01-28 20:18:04 -0500

    document name

---
+++ Lab3_DarianMarieIsMe_list.py
@@ -0,0 +1,7 @@
+'''Lab 3 Programming Assignment- Part 1
+Darian Marie Bruce
+The purpose of the first part of this assignment is to make a list
+that contains 10 items that I will be taking with me, show the number of
+items in that list and then sort and print the list alphabetically.
+01/28/2026
+'''
