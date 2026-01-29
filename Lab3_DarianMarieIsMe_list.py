76ddb666994d2f77c991cbe49ade6f5a4d9fa318
Darian Bruce <darianmariebruce@gmail.com>
2026-01-28 20:26:52 -0500

    finished part 1

--- Lab3_DarianMarieIsMe_list.py
+++ Lab3_DarianMarieIsMe_list.py
@@ -5,3 +5,26 @@ that contains 10 items that I will be taking with me, show the number of
 items in that list and then sort and print the list alphabetically.
 01/28/2026
 '''
+
+# list of 10 items
+
+items = ['shampoo', 'phone', 'charger', 'beer', 'chips', 'camper', 'towels', 'swimsuit', 'speaker', 'bike']
+
+# show number of items in list
+
+print("Number of items in the list:", len(items))
+
+# sort and print list alphabetically
+
+items.sort()
+
+print("Items I will be taking with me, sorted alphabetically:" items[0].title)
+print(items[1].title)
+print(items[2].title)
+print(items[3].title)
+print(items[4].title)
+print(items[5].title)
+print(items[6].title)
+print(items[7].title)
+print(items[8].title)
+print(items[9].title)
