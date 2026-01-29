ed19a93e2e21dab934ed3e18764956cbc6e798b2
Darian Bruce <darianmariebruce@gmail.com>
2026-01-28 20:39:38 -0500

    imported list

---
+++ Lab3_DarianMarieIsMe_add.py
@@ -0,0 +1,14 @@
+'''Lab 3 Programming Assignment- Part 2
+Darian Marie Bruce
+The purpose of the second part of this assignment is to import the list from
+part 1, append 5 more items to this list, and print the resulting list 
+reversed alphabetically.
+01/28/2026
+'''
+
+#Importing the list from Lab3_DarianMarieIsMe_list.py
+
+from Lab3.Lab3_DarianMarieIsMe_list import items
+
+#Appending 5 more items to the list
+
