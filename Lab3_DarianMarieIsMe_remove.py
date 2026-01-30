ab62332c6c6e59e0e65d369aba4a109b5a9dc12d
Darian Bruce <darianmariebruce@gmail.com>
2026-01-29 19:07:47 -0500

    created header, imported list, and then removed binoculars

---
+++ Lab3_DarianMarieIsMe_remove.py
@@ -0,0 +1,14 @@
+'''Lab 3 - Programming Assignment - Part 4
+Darian Marie Bruce
+The purpose of this assignment is to import the list from part 3, remove an item 
+from the list, print the resulting list, and print an f string afterwards
+01/29/2026 
+'''
+
+# Importing the list
+
+from Lab3_DarianMarieIsMe_replace import items
+
+# Removing an item from the list
+
+items.remove('binoculars')
